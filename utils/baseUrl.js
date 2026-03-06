2c71280fa60e30704cab14d804bf48ab6b30ff40
AhmadShopnil <ahmadshopnil75@gmail.com>
2026-03-07 03:26:14 +0600

    fix

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,8 +1,8 @@
 
-// export const BASE_URL = process.env.API_URL || 'https://importilabd.com'
+export const BASE_URL = process.env.API_URL || 'https://importilabd.com'
 
 
-export const BASE_URL = process.env.API_URL || 'http://localhost:3000'
+// export const BASE_URL = process.env.API_URL || 'http://localhost:3000'
 
 
 
